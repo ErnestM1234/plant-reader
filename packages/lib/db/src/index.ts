b6a85e51ce56850ffbcdb021418682715379fabe
ErnestM1234 <ernestm@princeton.edu>
2025-03-16 20:22:00 -0700

    switch to turbo build

--- packages/lib/db/src/index.ts
+++ packages/lib/db/src/index.ts
@@ -1,6 +1,11 @@
-import * as Postgres from '@prisma/client';
+import * as Postgres from "@prisma/client";
 
 let prisma: Postgres.PrismaClient;
 prisma = new Postgres.PrismaClient();
 
 export default prisma;
+
+export * from "./types";
+export { default as prisma } from "./client";
+export { default as getPlant } from "./plants/getPlant";
+export { default as getPlants } from "./plants/getPlants";
--- packages/lib/db/src/plants/getPlants.ts
+++ packages/lib/db/src/plants/getPlants.ts
@@ -1,17 +1,48 @@
-
-import prisma from '../index.js';
-import { Plant } from '../types.js';
-
+import prisma from "../client";
+import { Plant } from "../types";
 
 export default async function getPlants(): Promise<Plant[]> {
   try {
-      const plants = await prisma.plant.findMany();
-      if (!plants) {
-          return [];
-      }
-      return plants.map(({id, name, desc}) => {return {id, name, desc: desc || undefined, readers: []}});
+    const plants = await prisma.plant.findMany({
+      select: {
+        id: true,
+        name: true,
+        desc: true,
+        readers: {
+          select: {
+            id: true,
+            name: true,
+            metaData: true,
+            plantReaderLogs: {
+              select: {
+                id: true,
+                createdAt: true,
+                moistureLevel: true,
+                metaData: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    return plants.map((plant) => ({
+      id: plant.id,
+      name: plant.name,
+      desc: plant.desc || undefined,
+      readers: plant.readers.map((reader) => ({
+        id: reader.id,
+        name: reader.name,
+        metaData: (reader.metaData as Record<string, any>) || undefined,
+        plantReaderLogs: reader.plantReaderLogs.map((log) => ({
+          id: log.id,
+          createdAt: log.createdAt,
+          moistureLevel: log.moistureLevel,
+          metaData: (log?.metaData as Record<string, any>) || undefined,
+        })),
+      })),
+    }));
   } catch (error) {
-      console.error("Error in getPlant: ", error);
-      return [];
+    console.error("Error in getPlants: ", error);
+    return [];
   }
 }
